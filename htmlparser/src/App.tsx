97d9a992920019aa0dd4112c1f88e778eb19c0ef
sneko <supernekocat31@gmail.com>
2024-05-25 14:34:57 +0900

    fix:Fixed conditions under which FormResult component are rendered

--- htmlparser/src/App.tsx
+++ htmlparser/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
     return (
         <div className='max-w-5xl mx-auto p-8 text-center'>
             <Form onSubmit={handleSubmit} />
-            {Object.keys(formData).length > 0 && <FormResult formData={formData} />}
+            {formData.url !== '' && <FormResult formData={formData} />}
         </div>
     )
 }
