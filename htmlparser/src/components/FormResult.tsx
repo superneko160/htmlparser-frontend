8ab3bb69f4639a7c24f8f769e6a0adeaebd191e2
sneko <supernekocat31@gmail.com>
2024-11-19 22:14:05 +0900

    refactor:divide component(ResultTable)

--- htmlparser/src/components/FormResult.tsx
+++ htmlparser/src/components/FormResult.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import type { IndexFormData, AttributeValue } from './../types'
 import { useDataFetch } from './../hooks/useDataFetch'
 import Loading from './Loading'
+import ResultTable from './ResultTable'
 
 type FormResultProps = {
     formData: IndexFormData
@@ -31,42 +32,7 @@ function FormResult({ formData }: FormResultProps) {
                 ) : error ? (
                     <p className='text-red-500'>エラー: {error}</p>
                 ) : data ? (
-                    <div className='container p-2 mx-auto sm:p-4'>
-                        <div className='overflow-x-auto'>
-                            <table className='min-w-full text-xs'>
-                                <colgroup>
-                                    <col className='w-1/5' />
-                                    <col className='w-1/5' />
-                                    <col className='w-3/5' />
-                                </colgroup>
-                                <thead className='bg-gray-300'>
-                                    <tr className='text-center'>
-                                        <th className='p-3'>要素</th>
-                                        <th className='p-3'>属性</th>
-                                        <th className='p-3 text-left'>値</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {Object.entries(data.data).map(([element, attributes]) =>
-                                        attributes.map((attr: AttributeValue, index: number) =>
-                                            Object.entries(attr).map(([key, value]) => (
-                                                <tr
-                                                    key={`${element}-${index}-${key}`}
-                                                    className='border-b border-opacity-20 border-gray-400 bg-gray-50'
-                                                >
-                                                    <td className='p-3'>{element}</td>
-                                                    <td className='p-3'>{key}</td>
-                                                    <td className='p-3 text-left'>
-                                                        {value as string}
-                                                    </td>
-                                                </tr>
-                                            )),
-                                        ),
-                                    )}
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                    <ResultTable data={data} />
                 ) : (
                     <p>No Data</p>
                 )}
